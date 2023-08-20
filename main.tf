c32ad396830dbcb542b9219f9fc519571175e999
vzateychuk <vzateychuk@gmail.com>
2023-08-21 01:10:04 +0300

    module security groups

--- main.tf
+++ main.tf
@@ -1,25 +1,48 @@
-resource "aws_vpc" "web_dev_vpc" {
-  cidr_block = "10.0.0.0/16"
-}
-
-data "aws_ami" "aws_t3" {
+data "aws_ami" "app_ami" {
   most_recent = true
 
   filter {
-    name   = "image-id"
-    values = ["ami-0c4c4bd6cf0c5fe52"]
+    name   = "name"
+    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
   }
 
-  owners = ["137112412989"] # Amazon Linux 2023 AMI
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["979382823631"] # Bitnami
 }
 
 resource "aws_instance" "web" {
-  ami = data.aws_ami.aws_t3.id
+  ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+
+  vpc_security_group_ids = [ module.web_sg.security_group_id ]
 
   tags = {
     Name = "web"
-    Env = "dev"
+    Env  = "dev"
   }
-  
+
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+module "web_sg" {
+  name        = "web_server_sg"
+  description = "Allow http/ssh in. Allow everything out"
+  vpc_id      = data.aws_vpc.default.id
+
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+
+  ingress_rules       = ["http-80-tcp", "http-8080-tcp", "ssh-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules       = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+
 }
